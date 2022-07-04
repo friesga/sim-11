4e49fa71af8f7c6fb097ff26f97b68ffb995920c
Jos Fries <jos.fries@kpnmail.nl>
2022-07-04 12:13:51 +0200

    Complemented CmdLineOptions tests.

--- gtest/cmdlineoptionstest.cpp
+++ gtest/cmdlineoptionstest.cpp
@@ -3,18 +3,48 @@
 #include <gtest/gtest.h>
 
 // Define command line strings
-char *argv[] =
+char *argvSet0[] =
 {
     "sim-11",
-    "-h"
+    "-h",   // halt
+    "-b",   // bootstrap
+    "-z",   // compress
+    "-x",   // exit_on_halt
+    "-r",   // readOnly
+    "-n",   // createNewFile
+    "-q",   // quiet
+    "-l",   // load_file
+    "loadfile",
+    "-f",   // floppy_filename
+    "floppyfilename",
+    "-t",   // trace_file
+    "tracefile"
 };
 
-
-TEST (CmdLineOptionsTest, processOptions)
+TEST (CmdLineOptionsTest, optionsProcessed)
 {
     EXPECT_FALSE (CmdLineOptions::get().halt);
+    EXPECT_EQ (CmdLineOptions::get().floppy_filename, nullptr);
+    EXPECT_EQ (CmdLineOptions::get().load_file, nullptr);
+    EXPECT_FALSE (CmdLineOptions::get().bootstrap);
+    EXPECT_EQ (CmdLineOptions::get().trace_file, nullptr);
+    EXPECT_FALSE (CmdLineOptions::get().compress);
+    EXPECT_FALSE (CmdLineOptions::get().exit_on_halt);
+    EXPECT_FALSE (CmdLineOptions::get().readOnly);
+    EXPECT_FALSE (CmdLineOptions::get().quiet);
+    EXPECT_FALSE (CmdLineOptions::get().createNewFile);
+    EXPECT_FALSE (CmdLineOptions::get().openExistingFile);
 
-    CmdLineOptions::processOptions (sizeof (argv) /sizeof (argv[0]), argv);
+    CmdLineOptions::processOptions (sizeof (argvSet0) /sizeof (argvSet0[0]), argvSet0);
 
     EXPECT_TRUE (CmdLineOptions::get().halt);
+    EXPECT_TRUE (CmdLineOptions::get().bootstrap);
+    EXPECT_TRUE (CmdLineOptions::get().compress);
+    EXPECT_TRUE (CmdLineOptions::get().exit_on_halt);
+    EXPECT_TRUE (CmdLineOptions::get().readOnly);
+    EXPECT_TRUE (CmdLineOptions::get().quiet);
+    EXPECT_TRUE (CmdLineOptions::get().createNewFile);
+    EXPECT_FALSE (strcmp(CmdLineOptions::get().load_file, "loadfile"));
+    EXPECT_FALSE (strcmp(CmdLineOptions::get().floppy_filename, "floppyfilename"));
+    EXPECT_FALSE (strcmp(CmdLineOptions::get().trace_file, "tracefile"));
 }
