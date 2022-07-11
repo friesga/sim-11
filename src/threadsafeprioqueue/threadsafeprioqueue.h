a2d49f30146e125da8450d28263477b647ae4688
Jos Fries <jos.fries@kpnmail.nl>
2022-07-11 17:04:16 +0200

    Added private ThreadSafeContainer::front().

--- src/threadsafeprioqueue/threadsafeprioqueue.h
+++ src/threadsafeprioqueue/threadsafeprioqueue.h
@@ -5,12 +5,20 @@
 #include <mutex>
 #include <condition_variable>
 
+// Implementation of a thread safe queue. The queue is implemented by
+// means of a std::set because a set, contrary to a std::priority_queue,
+// provides an iterator and an erase() function. Because of the use of
+// std::set the queue can contain only unique elements. A std::set orders
+// its elements in ascending order. The element with the highest priority
+// therefore is at the end of the set.
 template <typename T>
 class ThreadSafePrioQueue
 {
     std::set<T> queue_;
     mutable std::mutex guard;
     std::condition_variable signal;
+
+    typename std::set<T>::iterator front ();
  
 public:
     void clear();
@@ -39,6 +47,7 @@ bool ThreadSafePrioQueue<T>::empty()
     return queue_.empty();
 }
 
+// Erase the element in the queue with the specified value
 template <typename T>
 void ThreadSafePrioQueue<T>::erase (T const &elem)
 {
@@ -58,11 +67,11 @@ bool ThreadSafePrioQueue<T>::fetchTop(T &dest)
         signal.wait(lock);
 
     // If this throws, nothing has been removed
-    dest = std::move (*(--queue_.end()));
+    dest = std::move (*front());
 
     // Once we reach this point we own the queue's top element and
     // can safely remove it
-    queue_.erase(--queue_.end());
+    queue_.erase(front());
 
     return true;
 }
@@ -86,7 +95,18 @@ template <typename T>
 T const &ThreadSafePrioQueue<T>::top()
 {
     std::lock_guard<std::mutex> lock(guard);
-    return *(--queue_.end());
+    return *front();
+}
+
+
+// Private functions
+// Return an iterator to the element at the front of the queue, i.e. the last
+// element in the set. This function is private as it relies on the lock of
+// the calling functions to keep the queue thread safe.
+template <typename T>
+typename std::set<T>::iterator ThreadSafePrioQueue<T>::front ()
+{
+    return --queue_.end();
 }
 
 #endif // !_THREADSAFEPRIOQUEUE_H_
