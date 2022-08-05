94a2c3c413861c49e70f7209fb67bc074193f835
Jos Fries <jos.fries@kpnmail.nl>
2022-08-05 17:12:34 +0200

    Added Write Check test.

---
+++ gtest/rlv12writechecktest.cpp
@@ -0,0 +1,131 @@
+#include "lsi11/lsi11.h"
+#include "msv11d/msv11d.h"
+#include "rlv12/rlv12.h"
+#include "cmdlineoptions/cmdlineoptions.h"
+
+#include <gtest/gtest.h>
+
+// Write to unit tests.
+
+// Definition of the test fixture
+class RLV12WriteCheckTest : public ::testing::Test
+{
+protected:
+    // Use our own set of definitions to avoid common cause issues
+    static constexpr u16 RLBASE = 0174400;
+    static constexpr u16 RLCSR = RLBASE;
+    static constexpr u16 RLBAR = RLBASE + 02;
+    static constexpr u16 RLDAR = RLBASE + 04;
+    static constexpr u16 RLMPR = RLBASE + 06;
+    // A 16 bit system has no BAE register
+
+    // CSR bit definitions
+    static constexpr u16  CSR_DriveReady          = 1;
+    static constexpr u16  CSR_WriteCheckCommand   = (01 << 1);
+    static constexpr u16  CSR_GetStatusCommand    = (02 << 1);
+    static constexpr u16  CSR_SeekCommand         = (03 << 1);
+    static constexpr u16  CSR_ReadHeaderCommand   = (04 << 1);
+    static constexpr u16  CSR_WriteDataCommand    = (05 << 1);
+    static constexpr u16  CSR_ReadDataCommand     = (06 << 1);
+    static constexpr u16  CSR_ControllerReady     = (1 << 7);
+    static constexpr u16  CSR_OperationIncomplete = (1 << 10); 
+    static constexpr u16  CSR_CompositeError      = (1 << 15); 
+    static constexpr u16  CSR_Drive0              = (0 << 8);
+    static constexpr u16  CSR_Drive1              = (1 << 8);
+    static constexpr u16  CSR_Drive2              = (2 << 8);
+    static constexpr u16  CSR_Drive3              = (3 << 8);
+
+    // DAR bit definitions
+    static constexpr u16 DAR_Marker    = (1 << 00);
+
+    // DAR bit definitions for a Get Status Command
+    static constexpr u16 DAR_GetStatus = (1 << 01);
+    static constexpr u16 DAR_Reset     = (1 << 03);
+
+    // DAR bit definitions for a Seek Command
+    static constexpr u16 DAR_Seek          = (0 << 01);
+    static constexpr u16 DAR_DirectionOut  = (1 << 02);
+    static constexpr u16 DAR_HeadSelect    = (1 << 04);
+    inline u16 DAR_cylinderDifference (u16 x) { return x << 07; }
+
+    // MPR bit definitions for a Seek Command
+    inline u16 MPR_cylinder (u16 x) { return x >> 07; }
+
+    // Create bus structure, an RLV12 device and install the device
+    QBUS bus;
+    MSV11D msv11;
+    RLV12 rlv12Device {};
+
+    void SetUp() override
+    {
+        // Create a minimal system, conisting of just the bus, memory
+        // and the RLV12 device to be tested.
+        bus.installModule (1, &msv11);
+        bus.installModule (2, &rlv12Device);
+
+        char const *argvSet0[] =
+        {
+            "sim-11",
+            "-q",   // quiet
+            "-n",   // createNewFile
+            "-o"    // ovewrite
+        };
+        CmdLineOptions::processOptions (sizeof (argvSet0) /sizeof (argvSet0[0]),
+            argvSet0);
+    }
+};
+
+// Verify the correct execution of Write Check command
+TEST_F (RLV12WriteCheckTest, writeCheckSucceeds)
+{
+    // Attach a new disk to unit 0
+    ASSERT_EQ (rlv12Device.unit (0)->attach ("rl01.dsk"), 
+        StatusCode::OK);
+
+    // Clear errors and volume check condition
+    rlv12Device.writeWord (RLDAR, DAR_Reset | DAR_GetStatus | DAR_Marker);
+    rlv12Device.writeWord (RLCSR, CSR_GetStatusCommand | CSR_Drive0);
+
+    // Fill 512 bytes of memory with the values to be written and a marker
+    u16 address;
+    for (address = 0; address < 512; address += 2)
+        bus.writeWord (address, 0177777);
+        
+    // Verify controller and drive are ready
+    u16 result;
+    rlv12Device.read (RLCSR, &result);
+    ASSERT_EQ (result & (CSR_ControllerReady | CSR_DriveReady), 
+        CSR_ControllerReady | CSR_DriveReady);
+
+    
+    // Write 256 words (i.e. 2 sectors)
+    rlv12Device.writeWord (RLBAR, 0);
+    rlv12Device.writeWord (RLDAR, 0);
+    rlv12Device.writeWord (RLMPR, 0xFF00);
+    rlv12Device.writeWord (RLCSR, CSR_WriteDataCommand);
+
+    // Wait for command completion
+    std::this_thread::sleep_for (std::chrono::milliseconds (500));
+
+    // Verify both controller and drive are ready and no error is
+    // indicated
+    rlv12Device.read (RLCSR, &result);
+    ASSERT_EQ (result & 
+        (CSR_CompositeError | CSR_ControllerReady | CSR_DriveReady),
+        CSR_ControllerReady | CSR_DriveReady);
+
+    // Issue the Write Check command
+    rlv12Device.writeWord (RLBAR, 0);
+    rlv12Device.writeWord (RLDAR, 0);
+    rlv12Device.writeWord (RLMPR, 0xFF00);
+    rlv12Device.writeWord (RLCSR, CSR_WriteCheckCommand);
+
+    // Wait for command completion
+    std::this_thread::sleep_for (std::chrono::milliseconds (500));
+
+    // Verify the Write Check indicates no error
+    rlv12Device.read (RLCSR, &result);
+    ASSERT_EQ (result & 
+        (CSR_CompositeError | CSR_ControllerReady | CSR_DriveReady),
+        CSR_ControllerReady | CSR_DriveReady);
+}
