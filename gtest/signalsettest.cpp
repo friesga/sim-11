17088193ff8fd5bb8d9a46b40124c5beeabb7e1b
Jos Fries <jos.fries@kpnmail.nl>
2023-02-20 12:06:38 +0100

    Added signal cycle unit test.

--- gtest/signalsettest.cpp
+++ gtest/signalsettest.cpp
@@ -10,7 +10,22 @@ TEST (SignalSetTest, signalCanBeSetAndTested)
     EXPECT_TRUE (!bus.signalIsSet (Qbus::Signal::BHALT));
 
     // Signals can be set and tested
-    bus.setSignal (Qbus::Signal::BHALT, true);
+    bus.setSignal (Qbus::Signal::BHALT, Qbus::SignalValue::True);
 
     EXPECT_TRUE (bus.signalIsSet (Qbus::Signal::BHALT));
+}
+
+TEST (SignalSetTest, signalCanBeCycled)
+{
+    Qbus bus;
+
+    // Signals should be false by default
+    EXPECT_FALSE (bus.signalIsSet (Qbus::Signal::BHALT));
+
+    // Signal is set to cycle
+    bus.setSignal (Qbus::Signal::BHALT, Qbus::SignalValue::Cycle);
+
+    // The first read should return true, the next read should return false
+    EXPECT_TRUE (bus.signalIsSet (Qbus::Signal::BHALT));
+    EXPECT_FALSE (bus.signalIsSet (Qbus::Signal::BHALT));
 }
