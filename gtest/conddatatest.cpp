533213786f22fa470156a99e8824bb9d7f133c01
Jos Fries <jos.fries@kpnmail.nl>
2023-03-27 09:57:42 +0200

    Added conddata unit-test and corrected CondData copy constructor and assignment operators.

---
+++ gtest/conddatatest.cpp
@@ -0,0 +1,102 @@
+#include "conddata/conddata.h"
+
+#include <gtest/gtest.h>
+#include <string>
+
+using std::string;
+
+TEST (CondDataTest, defaultConstructorCreatesEmptyObject)
+{
+    CondData<int> cd0;
+    EXPECT_TRUE (!cd0.hasValue ());
+}
+
+TEST (CondDataTest, accessingEmptyObjectThrows)
+{
+    CondData<int> cd0;
+    EXPECT_THROW (cd0.value (), string);
+}
+
+TEST (CondDataTest, valueOrCanAccessEmptyObject)
+{
+    CondData<int> cd0;
+    EXPECT_TRUE (cd0.valueOr (1) == 1);
+}
+
+TEST (CondDataTest, objectCanBeConstructedWithValue)
+{
+    CondData<int> cd0 {5};
+    EXPECT_TRUE (cd0.value () == 5);
+    EXPECT_TRUE (cd0.hasValue ());
+}
+
+TEST (CondDataTest, objectCanBeAssignedTo)
+{
+    CondData<int> cd0;
+    cd0 = 6;
+    EXPECT_TRUE (cd0.value () == 6);
+    EXPECT_TRUE (cd0.hasValue ());
+}
+
+TEST (CondDataTest, objectCanBeConvertedToNativeType)
+{
+    CondData<int> cd0 {6};
+    int tmp = cd0;
+    EXPECT_TRUE (tmp == 6);
+    EXPECT_TRUE (cd0 == 6);
+}
+
+// This test implicitly verifies that a CondData object can be converted to
+// the native type (as checked in the previous test).
+TEST (CondDataTest, objectCanBeAddedTo)
+{
+    CondData<int> cd0 {6};
+    cd0 += 4;
+    EXPECT_TRUE (cd0 == 10);
+}
+
+TEST (CondDataTest, objectCanBeSubtractedFrom)
+{
+    CondData<int> cd0 {6};
+    cd0 -= 2;
+    EXPECT_TRUE (cd0 == 4);
+}
+
+TEST (CondDataTest, objectCanBeSubtractedFromAndAssignedTo)
+{
+    CondData<int> cd0 {6};
+    cd0 -= 2;
+    EXPECT_TRUE (cd0 == 4);
+}
+
+TEST (CondDataTest, objectCanBeCopied)
+{
+    CondData<int> cd0 {6};
+    CondData<int> cd1;
+    cd1 = cd0;
+    EXPECT_TRUE (cd1 == 6);
+}
+
+TEST (CondDataTest, objectCanBeCopiedToAnotherType)
+{
+    CondData<short> cd0 {6};
+    CondData<int> cd1;
+    cd1 = cd0;
+    EXPECT_TRUE (cd1.hasValue ());
+}
+
+// The following two tests are not verifying CondData functionality.
+// In the tests a CondData object is converted to the native type on which
+// the operation is performed and then a CondData object is created from
+// the resulting integer.
+TEST (CondDataTest, objectCanBeMasked)
+{
+    CondData<int> cd0 = CondData<int>{3} & 0x1;
+    EXPECT_TRUE (cd0 == 1);
+}
+
+TEST (CondDataTest, objectCanBeShiftedRight)
+{
+    CondData<int> cd0 = CondData<int> {4} >> 2;
+    EXPECT_TRUE (cd0 == 1);
+}
--- src/conddata/conddata.h
+++ src/conddata/conddata.h
@@ -13,28 +13,30 @@ class CondData
 public:
     // Constructors
     CondData ();
-    CondData(T value);
-    CondData(T value, bool validValue);
+    CondData (T value);
+    CondData (T value, bool validValue);
+
+    // Copy constructor
     template <typename V>
-        CondData(CondData<V> other);
+        CondData (CondData<V> const &other);
 
     // Assignment operators
-    CondData<T>& operator= (T value);
-    CondData<T>& operator+= (T value);
-    CondData<T>& operator-= (T value);
+    CondData<T>& operator=  (T const &value);
+    CondData<T>& operator+= (T const &value);
+    CondData<T>& operator-= (T const &value);
 
     // Conversion operator
     operator T() const;
 
     // Accessors
-    bool hasValue() const;
-    T value() const;
-    T valueOr(T value) const;
+    bool hasValue () const;
+    T value () const;
+    T valueOr (T value) const;
 };
 
 // The default constructor constructs an object without valid data.
 template <typename T>
-inline CondData<T>::CondData()
+inline CondData<T>::CondData ()
     :
     value_{},
     validValue_{false}
@@ -42,7 +44,7 @@ inline CondData<T>::CondData()
 
 // Construct an object with valid data.
 template <typename T>
-inline CondData<T>::CondData(T value)
+inline CondData<T>::CondData (T value)
     :
     value_{value},
     validValue_{true}
@@ -50,27 +52,28 @@ inline CondData<T>::CondData(T value)
 
 // Construct an object with data and given validity.
 template <typename T>
-inline CondData<T>::CondData(T value, bool validValue)
+inline CondData<T>::CondData (T value, bool validValue)
     :
     value_{value},
     validValue_{validValue}
 {}
 
-// Construct a CondData object from a CondData object with another native type
+// Construct a CondData object from a CondData object with possibly another
+// native type.
 // The initialization of the data members is performed in the body of the
 // constructor as initialization via the initialization list with MSVC generates
 // compiler error C2397.
 template <typename T>
 template <typename V>
-inline CondData<T>::CondData(CondData<V> other)
+inline CondData<T>::CondData (CondData<V> const &other)
 {
-    value_ = static_cast<V> (other.valueOr(0));
-    validValue_ = other.hasValue();
+    value_ = static_cast<V> (other.valueOr (0));
+    validValue_ = other.hasValue ();
 }
 
 // Copy assign a native value
 template <typename T>
-inline CondData<T>& CondData<T>::operator= (T value)
+inline CondData<T>& CondData<T>::operator= (T const &value)
 {
     value_ = value;
     validValue_ = true;
@@ -79,23 +82,23 @@ inline CondData<T>& CondData<T>::operator= (T value)
 
 // Add native type to this object
 template <typename T>
-inline CondData<T>& CondData<T>::operator+= (T value)
+inline CondData<T>& CondData<T>::operator+= (T const &value)
 {
     if (validValue_)
         value_ += value;
     else
-        throw (std::string("Addition on nullopt"));
+        throw (std::string("Addition on invalid CondData object"));
     return *this;
 }
 
 // Subract native type from this object
 template <typename T>
-inline CondData<T>& CondData<T>::operator-= (T value)
+inline CondData<T>& CondData<T>::operator-= (T const &value)
 {
     if (validValue_)
         value_ -= value;
     else
-        throw (std::string("Addition on invalid CondData object"));
+        throw (std::string("Subtraction on invalid CondData object"));
     return *this;
 }
 
