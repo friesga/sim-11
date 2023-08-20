24105e32d55e7f971cec710a84faa2725620b720
Jos Fries <fries.ga@gmail.com>
2023-08-20 17:05:25 +0200

    Added (still unused) class NoOperandInstruction.

---
+++ src/kd11/cpu/nooperandinstruction/nooperandinstruction.cpp
@@ -0,0 +1,14 @@
+#include "nooperandinstruction.h"#include "markinstruction.h"
+
+NoOperandInstruction::NoOperandInstruction (CpuData* cpu, u16 instruction)
+    :
+    LSI11Instruction (cpu),
+    instr_ {instruction}
+{}
+
+// Return the instruction's operation code. The operation code takes up
+// the complete instruction.
+u16 NoOperandInstruction::getOperationCode ()
+{
+	return instr_;
+}
---
+++ src/kd11/cpu/nooperandinstruction/nooperandinstruction.h
@@ -0,0 +1,17 @@
+#ifndef _NOOPERANDINSTRUCTION_H_
+#define _NOOPERANDINSTRUCTION_H_
+
+#include "../lsi11instruction/lsi11instruction.h"
+#include "../kd11cpu.h"
+#include "../operandlocation/operandlocation.h"
+
+class NoOperandInstruction : public LSI11Instruction
+{
+	u16 instr_;
+
+public:
+	NoOperandInstruction (CpuData* cpu, u16 instruction);
+	u16 getOperationCode () override;
+};
+
+#endif // _NOOPERAND_H_
