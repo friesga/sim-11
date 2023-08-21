063d1b7f8bfb886b021fbc256a7234c5ee87cd3b
Jos Fries <jos.fries@kpnmail.nl>
2023-08-21 13:02:10 +0200

    Changed SBC to use readOperand() and writeOperand().

--- src/kd11/cpu/kd11_na_instructions/sbc.h
+++ src/kd11/cpu/kd11_na_instructions/sbc.h
@@ -26,27 +26,24 @@ namespace KD11_NA
     public:
         SBC (CpuData* cpu, u16 instruction);
         CpuData::Trap execute () override;
-
-    private:
-        OperandLocation location_;
     };
 
     SBC::SBC (CpuData* cpu, u16 instruction)
         :
-        SingleOperandInstruction (cpu, instruction),
-        location_ {getOperandLocation (cpu_->registers ())}
+        SingleOperandInstruction (cpu, instruction)
     {}
 
     CpuData::Trap SBC::execute ()
     {
-        CondData<u16> contents = location_.wordContents ();
-        if (!contents.hasValue ())
+        CondData<u16> contents;
+        if (!readOperand (&contents))
             return CpuData::Trap::BusError;
 
         u16 cBit = isSet (PSW_C) ? 1 : 0;
         u16 result = contents - cBit;
 
-        location_.write (result);
+        if (!writeOperand (result))
+            return CpuData::Trap::BusError;
 
         setConditionCodeIf_ClearElse (PSW_V, contents == 0100000);
         setConditionCodeIf_ClearElse (PSW_C, !contents && isSet (PSW_C));
