a28516fe4ef9122b42f3401fdc6001a3b5e87ca3
Jos Fries <jos.fries@kpnmail.nl>
2023-08-21 13:09:41 +0200

    Changed SWAB to use readOperand() and writeOperand().

--- src/kd11/cpu/kd11_na_instructions/swab.h
+++ src/kd11/cpu/kd11_na_instructions/swab.h
@@ -27,26 +27,24 @@ namespace KD11_NA
     public:
         SWAB (CpuData* cpu, u16 instruction);
         CpuData::Trap execute () override;
-
-    private:
-        OperandLocation location_;
     };
 
     SWAB::SWAB (CpuData* cpu, u16 instruction)
         :
-        SingleOperandInstruction (cpu, instruction),
-        location_ {getOperandLocation (cpu_->registers ())}
+        SingleOperandInstruction (cpu, instruction)
     {}
 
     CpuData::Trap SWAB::execute ()
     {
-        CondData<u16> operand = location_.wordContents ();
-        if (!operand.hasValue ())
+        CondData<u16> operand;
+        if (!readOperand (&operand))
             return CpuData::Trap::BusError;
 
         // Swap bytes in the operand and write it to the operand location
         operand = ((operand & 0x00FF) << 8) | ((operand >> 8) & 0xFF);
-        location_.write (operand);
+        
+        if (!writeOperand (operand))
+            return CpuData::Trap::BusError;
 
         setConditionCodeIf_ClearElse (PSW_N, operand & 0x80);
         setConditionCodeIf_ClearElse (PSW_Z, !((u8) operand));
