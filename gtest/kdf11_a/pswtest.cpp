96dcaf8d25879788b87b2993827a46ba29e4dd1a
Jos Fries <jos.fries@kpnmail.nl>
2023-10-13 16:18:31 +0200

    Moved common test code to a test fixture.

--- gtest/kdf11_a/pswtest.cpp
+++ gtest/kdf11_a/pswtest.cpp
@@ -4,84 +4,77 @@
 
 #include <gtest/gtest.h>
 
+class KDF11_A_PSWTEST : public ::testing::Test
+{
+protected:
+    Qbus qbus;
+    KDF11_A* kdf11a  = new KDF11_A (&qbus);
+    MSV11D*  memory  = new MSV11D (&qbus);
+    KDF11_AInstruction instrDecoder;
+    u16 const PswAddress = 0177776;
+
+    void SetUp() override
+    {
+        qbus.installModule (1, kdf11a);
+        qbus.installModule (2, memory);
+
+        kdf11a->start ();
+    }
+};
+
 // Verify the PSW can be accessed via address 0177776. Write all one's to
 // the PSW and expect all bits to be set except for the T-bit.
 //
-TEST (KDF11_A_PSWTEST, PswCanBeAccessedViaAddress)
+TEST_F (KDF11_A_PSWTEST, PswCanBeAccessedViaAddress)
 {
-    Qbus qbus;
-    KDF11_A kdf11_a (&qbus);
     u16 psw;
 
-    u16 const PswAddress = 0177776;
-
-    kdf11_a.start ();
-    kdf11_a.writeWord (PswAddress, 0177777);
-    EXPECT_EQ (kdf11_a.read (PswAddress, &psw), StatusCode::OK);
+    kdf11a->writeWord (PswAddress, 0177777);
+    EXPECT_EQ (kdf11a->read (PswAddress, &psw), StatusCode::OK);
     EXPECT_EQ (psw, 0177757);
 }
 
 
 // Verify that a MOV #0, @#PS instruction actually clears the PSW (and does
 // not set the Z-bit). See JKDBD0 test 33.
-TEST (KDF11_A_PSWTEST, MOVDoesNotSetCC)
+TEST_F (KDF11_A_PSWTEST, MOVDoesNotSetCC)
 {
-    Qbus qbus;
-    KDF11_A kdf11a (&qbus);
-    MSV11D memory (&qbus);
-    KDF11_AInstruction instrDecoder;
-    u16 const PswAddress = 0177776;
-
-    qbus.installModule (1, &memory);
-    qbus.installModule (2, &kdf11a);
-
-    kdf11a.start ();
-    kdf11a.writeWord (PswAddress, 0177777);
+    kdf11a->writeWord (PswAddress, 0177777);
 
     // MOVB #0, @#PS
-    unique_ptr<LSI11Instruction> instruction {instrDecoder.decode (&kdf11a.cpu (), 0012737)};
+    unique_ptr<LSI11Instruction> instruction {instrDecoder.decode (&kdf11a->cpu (), 0012737)};
 
     // Assume the MOV instruction is at address 0, so the second and third
     // word of the instruction are at address 2 and 4.
-    kdf11a.cpu ().registers () [7] = 2;
-    kdf11a.cpu ().putWord (2, 0);
-    kdf11a.cpu ().putWord (4, 0177776);
+    kdf11a->cpu ().registers () [7] = 2;
+    kdf11a->cpu ().putWord (2, 0);
+    kdf11a->cpu ().putWord (4, 0177776);
 
     EXPECT_EQ (instruction->execute (), CpuData::Trap::None);
     
     u16 psw;
-    EXPECT_EQ (kdf11a.read (PswAddress, &psw), StatusCode::OK);
+    EXPECT_EQ (kdf11a->read (PswAddress, &psw), StatusCode::OK);
     EXPECT_EQ (psw, 0);
 }
 
 
 // Verify that a CLR @#PS instruction actually clears the PSW (and does
 // not set the Z-bit). See JKDBD0 test 243.
-TEST (KDF11_A_PSWTEST, CLRDoesNotSetCC)
+TEST_F (KDF11_A_PSWTEST, CLRDoesNotSetCC)
 {
-    Qbus qbus;
-    KDF11_A kdf11a (&qbus);
-    MSV11D memory (&qbus);
-    KDF11_AInstruction instrDecoder;
-    u16 const PswAddress = 0177776;
-
-    qbus.installModule (1, &memory);
-    qbus.installModule (2, &kdf11a);
-
-    kdf11a.start ();
-    kdf11a.writeWord (PswAddress, 0177777);
+    kdf11a->writeWord (PswAddress, 0177777);
 
     // CLR @#PS
-    unique_ptr<LSI11Instruction> instruction {instrDecoder.decode (&kdf11a.cpu (), 0005037)};
+    unique_ptr<LSI11Instruction> instruction {instrDecoder.decode (&kdf11a->cpu (), 0005037)};
 
     // Assume the CLR instruction is at address 0, so the second word of the
     // instruction is at address 2.
-    kdf11a.cpu ().registers () [7] = 2;
-    kdf11a.cpu ().putWord (2, 0177776);
+    kdf11a->cpu ().registers () [7] = 2;
+    kdf11a->cpu ().putWord (2, 0177776);
 
     EXPECT_EQ (instruction->execute (), CpuData::Trap::None);
     
     u16 psw;
-    EXPECT_EQ (kdf11a.read (PswAddress, &psw), StatusCode::OK);
+    EXPECT_EQ (kdf11a->read (PswAddress, &psw), StatusCode::OK);
     EXPECT_EQ (psw, 0);
 }
