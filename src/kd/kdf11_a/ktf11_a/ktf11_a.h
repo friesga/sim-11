b36087c251977f460bcb25d3e873141432df5553
Jos Fries <jos.fries@kpnmail.nl>
2023-11-20 16:09:25 +0100

    Added comment on the KTF11-A.

--- src/kd/kdf11_a/ktf11_a/ktf11_a.h
+++ src/kd/kdf11_a/ktf11_a/ktf11_a.h
@@ -7,6 +7,17 @@
 // It implements a subset of the standard PDP-11 Memory Management. There is
 // no support for split I/D space and no supervisor mode.
 //
+// The memory management unit is located between the central processor unit
+// and the LSI-II bus address lines. When the memory management unit is
+// operating, the normal 16-bit direct byte address is no longer interpreted
+// as a direct physical address (PA) but as a virtual address (VA) containing
+// information to be used in constructing a new 18- or 22-bit physical
+// address. The information contained in the virtual address (VA) is combined
+// with relocation information to yield an 18- or 22-bit physical address (PA).
+// Using the memory management unit, memory can be dynamically allocated in
+// pages, each page composed of from 1 to 128 integral blocks of 32 words. 
+// (EK-KDF11-UG-PR2)
+//
 class KTF11_A
 {
 public:
