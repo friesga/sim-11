dd867218c32e2d6aa22fac58c574312876184730
Jos Fries <jos.fries@kpnmail.nl>
2023-11-27 16:14:00 +0100

    Added read-only test for SR2.

--- gtest/ktf11_a/ktf11_aregisterstest.cpp
+++ gtest/ktf11_a/ktf11_aregisterstest.cpp
@@ -74,3 +74,17 @@ TEST_F (KTF11_ARegisters, kernelDSpaceNotPresent)
     u16 value {0177777};
     EXPECT_EQ (ktf11_a.read (0172320, &value), StatusCode::NonExistingMemory);
 }
+
+// Verify SR2 is read-only
+TEST_F (KTF11_ARegisters, statusRegister2IsReadOnly)
+{
+    // Verify SR2 initially has value 0
+    u16 value {0177777};
+    EXPECT_EQ (ktf11_a.read (01777576, &value), StatusCode::OK);
+    EXPECT_EQ (value, 0);
+
+    // Verify a new value cannot be written
+    EXPECT_EQ (ktf11_a.writeWord (01777576, 0177777), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.read (01777576, &value), StatusCode::OK);
+    EXPECT_EQ (value, 0);
+}
