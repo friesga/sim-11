60d6a8d3bafbf40a7af201616fdfb59a68191849
Jos Fries <jos.fries@kpnmail.nl>
2023-11-29 08:55:11 +0100

    Defined constants for register addresses in KTF11_ARegisters test.

--- gtest/ktf11_a/ktf11_aregisterstest.cpp
+++ gtest/ktf11_a/ktf11_aregisterstest.cpp
@@ -8,6 +8,12 @@
 class KTF11_ARegisters : public ::testing::Test
 {
 protected:
+    static const int SR0 {01777572};
+    static const int SR1 {01777574};
+    static const int SR2 {01777576};
+    static const int KernelPar7 {0172356};
+    static const int KernelDspacePDR0 {0172320};
+
     Qbus qbus;
     KDF11_A_Cpu kdf11_aCpu {&qbus};
     KTF11_A ktf11_a {&qbus, &kdf11_aCpu};
@@ -18,12 +24,12 @@ TEST_F (KTF11_ARegisters, statusRegister0)
 {
     // Verify SR0 initially has value 0
     u16 value {0177777};
-    EXPECT_EQ (ktf11_a.read (01777572, &value), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.read (SR0, &value), StatusCode::OK);
     EXPECT_EQ (value, 0);
 
     // Verify a new value can be written
-    EXPECT_EQ (ktf11_a.writeWord (01777572, 0177777), StatusCode::OK);
-    EXPECT_EQ (ktf11_a.read (01777572, &value), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.writeWord (SR0, 0177777), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.read (SR0, &value), StatusCode::OK);
     EXPECT_EQ (value, 0177777);
 }
 
@@ -33,12 +39,12 @@ TEST_F (KTF11_ARegisters, statusRegister1)
 {
     // Verify SR0 initially has value 0
     u16 value {0177777};
-    EXPECT_EQ (ktf11_a.read (01777574, &value), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.read (SR1, &value), StatusCode::OK);
     EXPECT_EQ (value, 0);
 
     // Verify a new value cannot be written
-    EXPECT_EQ (ktf11_a.writeWord (01777574, 0177777), StatusCode::OK);
-    EXPECT_EQ (ktf11_a.read (01777574, &value), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.writeWord (SR1, 0177777), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.read (SR1, &value), StatusCode::OK);
     EXPECT_EQ (value, 0);
 }
 
@@ -58,16 +64,16 @@ TEST_F (KTF11_ARegisters, kernelPAR7)
 {
     // Verify PAR initially has value 0
     u16 value {0177777};
-    EXPECT_EQ (ktf11_a.read (0172356, &value), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.read (KernelPar7, &value), StatusCode::OK);
     EXPECT_EQ (value, 0);
 
     // Verify a new value can be written
-    EXPECT_EQ (ktf11_a.writeWord (0172356, 0177777), StatusCode::OK);
-    EXPECT_EQ (ktf11_a.read (0172356, &value), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.writeWord (KernelPar7, 0177777), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.read (KernelPar7, &value), StatusCode::OK);
     EXPECT_EQ (value, 0177777);
 }
 
-// Verify Kernel D space PDR is not present
+// Verify Kernel D space PDR 0 is not present
 TEST_F (KTF11_ARegisters, kernelDSpaceNotPresent)
 {
     Qbus qbus;
@@ -75,7 +81,7 @@ TEST_F (KTF11_ARegisters, kernelDSpaceNotPresent)
     KTF11_A ktf11_a {&qbus, &kdf11_aCpu};
 
     u16 value {0177777};
-    EXPECT_EQ (ktf11_a.read (0172320, &value), StatusCode::NonExistingMemory);
+    EXPECT_EQ (ktf11_a.read (KernelDspacePDR0, &value), StatusCode::NonExistingMemory);
 }
 
 // Verify SR2 is read-only
@@ -83,11 +89,11 @@ TEST_F (KTF11_ARegisters, statusRegister2)
 {
     // Verify SR2 initially has value 0
     u16 value {0177777};
-    EXPECT_EQ (ktf11_a.read (01777576, &value), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.read (SR2, &value), StatusCode::OK);
     EXPECT_EQ (value, 0);
 
     // Verify a new value cannot be written
-    EXPECT_EQ (ktf11_a.writeWord (01777576, 0177777), StatusCode::OK);
-    EXPECT_EQ (ktf11_a.read (01777576, &value), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.writeWord (SR2, 0177777), StatusCode::OK);
+    EXPECT_EQ (ktf11_a.read (SR2, &value), StatusCode::OK);
     EXPECT_EQ (value, 0);
 }
