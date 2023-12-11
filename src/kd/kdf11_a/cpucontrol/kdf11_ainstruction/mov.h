dbf0a2123e774a4999dd187ab88af803cf2310ed
Jos Fries <jos.fries@kpnmail.nl>
2023-12-11 13:00:19 +0100

    Moved cpucontrol/doubleoperandinstruction to kdf11_a.

--- src/kd/kdf11_a/cpucontrol/kdf11_ainstruction/mov.h
+++ src/kd/kdf11_a/cpucontrol/kdf11_ainstruction/mov.h
@@ -2,7 +2,7 @@
 #define _MOV_H_
 
 #include "kd/kdf11_a/cpucontrol/kdf11_ainstruction/kdf11_ainstruction.h"
-#include "kd/kdf11_a/cpucontrol/kd11doubleoperandinstruction/kd11doubleoperandinstruction.h"
+#include "kd/kdf11_a/kd11doubleoperandinstruction/kd11doubleoperandinstruction.h"
 #include "kd/include/cpudata.h"
 #include "kd/common/operandlocation/operandlocation.h"
 #include "kd/common/instructions/withfactory.h"
