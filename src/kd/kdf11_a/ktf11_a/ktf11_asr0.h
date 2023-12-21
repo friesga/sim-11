ada3d304f549a00cdb218a712d3cab58319c24d9
Jos Fries <jos.fries@kpnmail.nl>
2023-12-21 18:47:45 +0100

    Set abort condition only when abort condition bits are clear(ed).

--- src/kd/kdf11_a/ktf11_a/ktf11_asr0.h
+++ src/kd/kdf11_a/ktf11_a/ktf11_asr0.h
@@ -20,6 +20,8 @@ public:
     static const u16 AccessViolationModeMask = (u16) (bitField (2) << AccessViolationModeIndex);
     static const u16 PageNumberIndex = 1;
     static const u16 PageNumberMask = (u16) (bitField (3) << PageNumberIndex);
+    static const u16 AbortConditionIndex = 13;
+    static const u16 AbortConditionMask = (u16) (bitField (3) << AbortConditionIndex);
 
     SR0 (u16 value);
     bool managementEnabled ();
@@ -48,12 +50,21 @@ inline bool SR0::accessAborted ()
     return value_ & AbortFlagsMask;
 }
 
-
+// Report the given abort condition in Status Register 0. Bit 0 will be set as
+// otherwise the access wouldn't have been aborted.
+// 
+// After an MMU abort has occurred, the program must clear SR0<15:13> in order
+// to resume monitoring memory management status. (EK-KDF11-UG-PR2)
+// This implies that an error must only be set when bits <15:13) are clear.
 inline void SR0::setAbortCondition (AbortReason reason, u16 mode, u16 pageNumber)
 {
-    value_ = static_cast<u16> (reason) |
-        (mode << AccessViolationModeIndex) & AccessViolationModeMask |
-        (pageNumber << PageNumberIndex) & PageNumberMask;
+    if ((value_ & AbortConditionMask) == 0)
+    {
+        value_ = static_cast<u16> (reason) |
+            (mode << AccessViolationModeIndex) & AccessViolationModeMask |
+            (pageNumber << PageNumberIndex) & PageNumberMask |
+            EnableManagementMask;
+    }
 }
 
 #endif // _KTF11ASR0_H_
