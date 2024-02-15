fd90797d571051ae4ba0ad828fc3683d777d2ad0
Jos Fries <jos.fries@kpnmail.nl>
2024-02-15 17:22:14 +0100

    Added deafult SLUConfig to KDF11-B configuration.

--- src/configdata/kdf11_b/kdf11_bconfig/kdf11_bconfig.cpp
+++ src/configdata/kdf11_b/kdf11_bconfig/kdf11_bconfig.cpp
@@ -1,6 +1,10 @@
 #include "kdf11_bconfig.h"
 
+using std::make_unique;
+
 KDF11_BConfig::KDF11_BConfig ()
     : 
     KD11Config (DeviceType::KDF11_B)
-{}
+{
+    sluConfig = make_unique<SLUConfig> ();
+}
