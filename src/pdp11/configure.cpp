a9005e2c01e61c304d7d6662bdd568343880d4be
Jos Fries <jos.fries@kpnmail.nl>
2024-03-25 12:26:23 +0100

    Replaced switch on variant index by visitors.

--- src/pdp11/configure.cpp
+++ src/pdp11/configure.cpp
@@ -36,6 +36,12 @@ void PDP_11::configureDevices (Window *window)
     reset ();
 }
 
+// ”Inherit” all function call operators of passed base types
+template<typename... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+
+// explicit deduction guide (not needed as of C++20)
+// template<typename... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
 // Configure the devices and install them on the bus.
 // Accessing a non-configured device will result in a bus time-out
 // and the BDV11 boot will halt at address 000010.
@@ -45,68 +51,37 @@ void PDP_11::configureDevices (vector<DeviceConfigVariant> systemConfig,
     // Check for presence of necessary devices
     checkConsistency (systemConfig);
 
-    // Get the device configurations and populate the LSI bus with these devices.
-    for (auto device : systemConfig)
+    auto configVisitor = overloaded 
     {
-        switch (device.index ())
-        {
-            // case DeviceType::KD11_NA:
-            case 0:
-                processor_ = new KD11_NA (&bus_,
-                    get<shared_ptr<KD11_NAConfig>> (device));
-                break;
+        [this] (shared_ptr<KD11_NAConfig> kd11_naConfig)
+            {processor_ = new KD11_NA (&bus_, kd11_naConfig); },
+        [this] (shared_ptr<KDF11_AConfig> kdf11_aConfig)
+            {processor_ = new KDF11_A (&bus_, kdf11_aConfig); },
+        [this] (shared_ptr<KDF11_BConfig> kdf11_bConfig)
+            {processor_ = new KDF11_B (&bus_, kdf11_bConfig); },
+        [this] (shared_ptr<MSV11Config> msv11Config)
+            {msv11_.push_back (new MSV11D (&bus_, msv11Config));},
+        [this] (shared_ptr<DLV11JConfig> dlv11jConfig)
+            {dlv11_ = new DLV11J (&bus_, dlv11jConfig); },
+        [this] (shared_ptr<BDV11Config> bdv11Config)
+            {bdv11_ = new BDV11 (&bus_, bdv11Config); },
+        [this] (shared_ptr<RXV21Config> rxv21Config)
+            {rxv21_ = new RXV21 (&bus_, rxv21Config); },
+        [this] (shared_ptr<RLConfig> rlConfig)
+            {rlv12_ = new RLV12 (&bus_, rlConfig); },
+        [this, window] (shared_ptr<BA11_NConfig> ba11_nConfig) 
+            {ba11_n_ = std::make_unique<BA11_N> (&bus_, window, ba11_nConfig); },
+        [this] (shared_ptr<RXV21UnitConfig> rxv21UnitConfig)
+            { throw std::runtime_error ("RXV21UnitConfig in systemConfig"); },
+        [this] (shared_ptr<RLUnitConfig> rlUnitConfig)
+            { throw std::runtime_error ("RLUnitConfig in systemConfig"); },
+        [this] (shared_ptr<SLUConfig> sluConfig)
+            { throw std::runtime_error ("SLUConfig in systemConfig"); },
+    };
 
-            // case DeviceType::KDF11_A:
-            case 1:
-                processor_ = new KDF11_A (&bus_,
-                    get<shared_ptr<KDF11_AConfig>> (device));
-                break;
-
-            // case DeviceType::KDF11_B:
-            case 2:
-                processor_ = new KDF11_B (&bus_,
-                    get<shared_ptr<KDF11_BConfig>> (device));
-                break;
-
-            // case DeviceType::MSV11:
-            case 3:
-                msv11_.push_back (new MSV11D (&bus_,
-                    get<shared_ptr<MSV11Config>> (device)));
-                break;
-
-            // case DeviceType::DLV11_J:
-            case 4:
-                dlv11_ = new DLV11J (&bus_, 
-                    get<shared_ptr<DLV11JConfig>> (device));
-                break;
-
-            // case DeviceType::BDV11:
-            case 5:
-                bdv11_ = new BDV11 (&bus_,
-                    get<shared_ptr<BDV11Config>> (device));
-                break;
-
-            // case DeviceType::RXV21:
-            case 6:
-                rxv21_ = new RXV21 (&bus_,
-                    get<shared_ptr<RXV21Config>> (device));
-                break;
-
-            // case DeviceType::RLV12:
-            case 7:
-                rlv12_ = new RLV12
-                    (&bus_, get<shared_ptr<RLConfig>> (device));
-                break;
-
-            // case DeviceType::BA11_N:
-            case 8:
-                ba11_n_ = std::make_unique<BA11_N> (&bus_, window, 
-                    get<shared_ptr<BA11_NConfig>> (device));
-                break;
-
-            default:
-                throw "Unknown device type in configuration";
-        }
+    for (DeviceConfigVariant deviceConfigVariant : systemConfig)
+    {
+        visit (configVisitor, deviceConfigVariant);
     }
 
     installModules ();
