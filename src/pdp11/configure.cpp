b82f204bf86f3378dcab8b55851e0dbe688bb45f
Jos Fries <jos.fries@kpnmail.nl>
2024-11-14 17:31:16 +0100

    Added MS11-P to configVisitor.

--- src/pdp11/configure.cpp
+++ src/pdp11/configure.cpp
@@ -76,6 +76,8 @@ void PDP_11::configureDevices (vector<DeviceConfig> systemConfig,
             {rlv12_ = new RLV12 (&bus_, window, rlConfig);},
         [this, window] (shared_ptr<BA11_NConfig> ba11_nConfig) 
             {ba11_n_ = std::make_unique<BA11_N> (&bus_, window, ba11_nConfig);},
+        [this] (shared_ptr<MS11PConfig> ms11pConfig)
+            {/* Ignore MS11P for now */ }
     };
 
     for (DeviceConfig deviceConfigVariant : systemConfig)
