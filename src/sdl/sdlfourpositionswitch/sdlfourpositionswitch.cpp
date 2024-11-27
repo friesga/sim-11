7a8dd9dd0fc18ba236625c7069fe31c77135be1c
Jos Fries <fries.ga@gmail.com>
2024-11-27 16:45:05 +0100

    Added class SDLFourPositionSwitch.

---
+++ src/sdl/sdlfourpositionswitch/sdlfourpositionswitch.cpp
@@ -0,0 +1,59 @@
+#include "sdlfourpositionswitch.h"
+
+using std::make_unique;
+using std::get;
+
+SDLFourPositionSwitch::SDLFourPositionSwitch (array<string, 4> positionImageNames,
+    Button::FourPositionsState initialState,
+    unique_ptr<SDLRenderer>& sdlRenderer,
+    EventCallback switchClicked,
+    SDL_Texture* targetTexture,
+    Frame<int> frame)
+    :
+    switchPosition_ {initialState},
+    sdlRenderer_ {sdlRenderer},
+    switchClicked_ {switchClicked},
+    frame_ {frame}
+{
+    for (auto imageName : positionImageNames)
+    {
+        positionTextures_.emplace_back (make_unique<SDLTexture> (imageName,
+            sdlRenderer->getSDL_Renderer (), targetTexture, frame));
+    }
+}
+
+void SDLFourPositionSwitch::setState (State newState)
+{
+    switchPosition_ = get<Button::FourPositionsState> (newState);
+}
+
+void SDLFourPositionSwitch::render ()
+{
+    // positionTextures_[static_cast<int> (state_)]->render ();
+    positionTextures_[+switchPosition_]->render ();
+}
+
+void SDLFourPositionSwitch::handleEvent (InputEvent const* event)
+{
+    if (event->type () == InputEvent::Type::MouseButtonDown &&
+        event->button () == InputEvent::Button::Left &&
+        isWithinBounds (event->mousePosition (), 0.75f))
+    {
+        switchPosition_ = nextPosition (switchPosition_);
+        switchClicked_ (switchPosition_);
+    }
+}
+
+bool SDLFourPositionSwitch::isWithinBounds (Position position, float margin)
+{ 
+    return positionTextures_[+switchPosition_]->isWithinBounds (position, margin);
+}
+
+Button::FourPositionsState SDLFourPositionSwitch::nextPosition (Button::FourPositionsState position)
+{
+    // return static_cast<Button::FourPositionsState> (static_cast<int> (position) + 1);
+    if (switchPosition_ != Button::FourPositionsState::P3)
+        return static_cast<Button::FourPositionsState> ((+position) + 1);
+
+    return Button::FourPositionsState::P3;
+}
---
+++ src/sdl/sdlfourpositionswitch/sdlfourpositionswitch.h
@@ -0,0 +1,56 @@
+#ifndef _SDLFOURPOSITIONSWITCH_H_
+#define _SDLFOURPOSITIONSWITCH_H_
+
+#include "panel.h"
+#include "../sdltexture/sdltexture.h"
+#include "../sdlrenderer/sdlrenderer.h"
+
+#include <string>
+#include <array>
+#include <vector>
+#include <memory>
+
+using std::string;
+using std::array;
+using std::vector;
+using std::unique_ptr;
+
+// Definition of operator+ for scoped enums to convert it to the underlying
+// type.
+// Source: https://stackoverflow.com/questions/8357240/how-to-automatically-convert-strongly-typed-enum-into-int
+//
+template <typename T>
+constexpr auto operator+ (T e) noexcept
+-> std::enable_if_t<std::is_enum<T>::value, std::underlying_type_t<T>>
+{
+    return static_cast<std::underlying_type_t<T>> (e);
+}
+
+class SDLFourPositionSwitch : public Button
+{
+public:
+    SDLFourPositionSwitch (array<string, 4> positionImages,
+        Button::FourPositionsState initialState,
+        unique_ptr<SDLRenderer>& sdlRenderer,
+        EventCallback switchClicked,
+        SDL_Texture* targetTexture,
+        Frame<int> frame);
+
+    // Functions required for the Button interface
+    void setState (State newState);
+    void render ();
+    void handleEvent (InputEvent const* event);
+    bool isWithinBounds (Position position, float margin);
+
+private:
+    unique_ptr<SDLRenderer>& sdlRenderer_;
+    EventCallback switchClicked_;
+    Frame<int> frame_;
+
+    Button::FourPositionsState switchPosition_;
+    vector<unique_ptr<SDLTexture>> positionTextures_;
+
+    Button::FourPositionsState nextPosition (Button::FourPositionsState position);
+};
+
+#endif // _SDLFOURPOSITIONSWITCH_H_
