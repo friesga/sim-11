6146c0a44a85a451b488420cefb44a99791d0f92
Jos Fries <jos.fries@kpnmail.nl>
2024-12-10 13:31:44 +0100

    Added comment on Unibus/QBus bus reads and writes.

--- src/qbus/qbus.cpp
+++ src/qbus/qbus.cpp
@@ -10,6 +10,14 @@ using std::placeholders::_1;
 
 #define	IRCJITTER()	(rand() % INTRPT_LATENCY_JITTER)
 
+// On the Unibus and QBus reads are always word-sized (and from an even
+// address) and writes may be either word or byte sized (with byte-sized
+// writes using the upper or lower byte depending on whether the address
+// is odd or even). When the CPU needs a byte it reads a word and ignores
+// the portion of the word it isn't interested in.
+// 
+// Source: https://retrocomputing.stackexchange.com/questions/13262/how-movb-tstb-and-all-byte-instructions-works-in-odd-address-read-in-pdp11
+//
 Qbus::Qbus ()
 	:
 	processorRunning_ {false},
