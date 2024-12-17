14c86e51068c6b2f91a4f1d7b2fab21cf7afe08a
Jos Fries <jos.fries@kpnmail.nl>
2024-12-17 11:07:31 +0100

    Set WIndows console to VT100 mode.

--- src/console/operatorconsole/windowsconsole/sender.cpp
+++ src/console/operatorconsole/windowsconsole/sender.cpp
@@ -6,6 +6,7 @@
 void WindowsConsole::sender ()
 {
     HANDLE stdInputHandle;
+    HANDLE stdOutputHandle;
 
     consoleRunning_ = true;
 
@@ -17,6 +18,16 @@ void WindowsConsole::sender ()
     // Set console in raw mode
     if (!SetConsoleMode (stdInputHandle, 0))
         throw std::string("Setting console mode failure");
+
+    // Get handle to stdout
+    if ((stdOutputHandle =
+        GetStdHandle (STD_OUTPUT_HANDLE)) == INVALID_HANDLE_VALUE)
+        throw std::string ("Cannot get console output handle");
+
+    // Set console to VT100 mode.
+    if (!SetConsoleMode (stdOutputHandle,
+            ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING))
+        throw std::string ("Setting console mode failure");
 
     while (consoleRunning_)
     {
