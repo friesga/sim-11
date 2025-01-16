9710676b9255f13e70230d176f9d0986954ddad6
Jos Fries <jos.fries@kpnmail.nl>
2025-01-16 10:30:58 +0100

    Moved read comment to read.cpp.

--- src/bus/qbus/qbus.cpp
+++ src/bus/qbus/qbus.cpp
@@ -7,14 +7,6 @@
 using std::bind;
 using std::placeholders::_1;
 
-// On the Unibus and QBus reads are always word-sized (and from an even
-// address) and writes may be either word or byte sized (with byte-sized
-// writes using the upper or lower byte depending on whether the address
-// is odd or even). When the CPU needs a byte it reads a word and ignores
-// the portion of the word it isn't interested in.
-// 
-// Source: https://retrocomputing.stackexchange.com/questions/13262/how-movb-tstb-and-all-byte-instructions-works-in-odd-address-read-in-pdp11
-//
 Qbus::Qbus ()
 {
 	ourKey_ = BINIT().subscribe (bind (&Qbus::BINITReceiver, this, _1));
