05c71b0b2beada47a1e77e24c45b7a008d98c33e
Jos Fries <jos.fries@kpnmail.nl>
2025-01-22 11:16:33 +0100

    Added exception for the mapping of addresses in I/O page.

--- src/bus/unibus/unibus.cpp
+++ src/bus/unibus/unibus.cpp
@@ -21,12 +21,13 @@ void Unibus::BINITReceiver (bool signalValue)
 
 // A given address must be mapped if:
 // - A Unibus Map option is present, and
+// - The address is not in the I/O page, and
 // - The address is a 18-bit address, or
 // - The address is a 22-bit address with the highest four bits set.
 bool Unibus::addressMustBeMapped (BusAddress address)
 {
-	return unibusMap_ != nullptr &&
-		(address.width () == BusAddress::Width::_18Bit) ||
+	return unibusMap_ != nullptr && !address.isInIOpage () &&
+		((address.width () == BusAddress::Width::_18Bit) ||
 		(address.width () == BusAddress::Width::_22Bit &&
-			(static_cast<u32> (address) & 017000000) == 017000000);
+			(static_cast<u32> (address) & 017000000) == 017000000));
 }
