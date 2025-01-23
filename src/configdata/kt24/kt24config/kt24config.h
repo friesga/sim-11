d8189386ec9c86fb440b00dd3c9a5ec86b5e4f50
Jos Fries <jos.fries@kpnmail.nl>
2025-01-23 13:28:36 +0100

    Added comment on the KT24 configuration options.

--- src/configdata/kt24/kt24config/kt24config.h
+++ src/configdata/kt24/kt24config/kt24config.h
@@ -8,6 +8,13 @@
 
 using std::shared_ptr;
 
+// The KT24 principally comprises two devices the Unibus Map logic
+// and a M9312 (compatible) device. The KT24 therefore has all M9312
+// options. Apart from the M9312 options the KT24 also has a "Diagnostic
+// ROM enabled" option and jumpers to set adressing limits. The
+// "Diagnostic ROM enabled" option is implemented via the "diag-rom"
+// option. Addressing limits currently aren't implemented.
+//
 struct KT24Config : public DeviceType<BusType::Unibus>
 {
     shared_ptr<M9312Config> m9312ConfigPtr {};
