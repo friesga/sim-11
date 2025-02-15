012e1d8b1aca05f2bd7a0c56ba34a028f0468884
Jos Fries <jos.fries@kpnmail.nl>
2025-02-15 09:00:38 +0100

    Improved output of MS11-P trace records.

--- src/trace/ms11precord/operator.cpp
+++ src/trace/ms11precord/operator.cpp
@@ -8,11 +8,11 @@ std::ostream& operator<< (std::ostream& os, TraceRecord<MS11_PRecord> record)
     switch (record.type_)
     {
         case MS11_PRecordType::ReadCSR:
-            os << "[MS1P] read CSR" << oct << record.csr_ << '\n';
+            os << "[MS1P] read CSR: " << oct << record.csr_ << '\n';
             break;
 
         case MS11_PRecordType::WriteCSR:
-            os << "[MS1P] write CSR" << oct << record.csr_ << '\n';
+            os << "[MS1P] write CSR: " << oct << record.csr_ << '\n';
             break;
 
         case MS11_PRecordType::ReadMemory:
