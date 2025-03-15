98ddbd2b6c648e38e98960f10fd41cea33ed8c7e
Jos Fries <jos.fries@kpnmail.nl>
2025-03-15 13:24:19 +0100

    Enhanced CpuEventRecordType output.

--- src/trace/cpueventrecord/operator.cpp
+++ src/trace/cpueventrecord/operator.cpp
@@ -5,24 +5,35 @@ std::ostream& operator<< (std::ostream& os, TraceRecord<CpuEventRecord> record)
     switch (record.type_)
     {
         case CpuEventRecordType::CPU_TRAP:
-            os << "[KD11_NA] handling trap: " << oct << record.value_ << '\n';
+            os << "[KD11] handling trap: " << oct << record.value_ << '\n';
             break;
 
         case CpuEventRecordType::CPU_HALT:
+            os << "[KD11] CPU_HALT @ " << oct << record.value_ << '\n';
+            break;
+
         case CpuEventRecordType::CPU_WAIT:
-            os << "[KD11_NA] HALT @ " << oct << record.value_ << '\n';
+            os << "[KD11] CPU_WAIT @ " << oct << record.value_ << '\n';
             break;
 
         case CpuEventRecordType::CPU_RUN:
-            os << "[KD11_NA] resume execution at " << oct << record.value_ << '\n';
+            os << "[KD11] resume execution at " << oct << record.value_ << '\n';
             break;
 
         case CpuEventRecordType::CPU_DBLBUS:
-            os << "[KD11_NA] double bus error [" << oct << record.value_ << "]\n";
+            os << "[KD11] double bus error [" << oct << record.value_ << "]\n";
+            break;
+
+        case CpuEventRecordType::CPU_ODT_P:
+            os << "[KD11] ODT Proceed " << oct << record.value_ << '\n';
+            break;
+
+        case CpuEventRecordType::CPU_ODT_G:
+            os << "[KD11] ODT Go " << oct << record.value_ << '\n';
             break;
 
         default:
-            os << "[KD11_NA] CpuEventRecordType :" << static_cast<int> (record.type_) << '\n';
+            os << "[KD11] CpuEventRecordType :" << static_cast<int> (record.type_) << '\n';
             break;
     }
     return os;
