ea8eed9bcda7a6031ea5e7978eebd0b0413098be
Jos Fries <jos.fries@kpnmail.nl>
2025-04-23 10:55:56 +0200

    Added registers initialization test.

--- gtest/rk/rk11dregisterstest.cpp
+++ gtest/rk/rk11dregisterstest.cpp
@@ -21,3 +21,17 @@ TEST (RK11DRegistersTest, controllerIsResponsibleForDefaultAddress)
     busAddress = BusAddress {0x177420};
     EXPECT_FALSE (rk11dDevice->responsible (busAddress));
 }
+
+// Verify the registers are correcly initialised
+TEST (RK11DRegistersTest, registersInitialised)
+{
+    RK11DConfig rk11dConfig {};
+
+    Unibus bus;
+    RK11D* rk11dDevice = new RK11D (&bus, rk11dConfig);
+
+    EXPECT_EQ (rk11dDevice->read (BusAddress {0177402}), 0);
+    EXPECT_EQ (rk11dDevice->read (BusAddress {0177406}), 0);
+    EXPECT_EQ (rk11dDevice->read (BusAddress {0177410}), 0);
+    EXPECT_EQ (rk11dDevice->read (BusAddress {0177416}), 0);
+}
