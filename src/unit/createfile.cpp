0bc7a89ac0a4576d7a3295028feb6bc5924810df
Jos Fries <jos.fries@kpnmail.nl>
2025-05-07 10:59:01 +0200

    Corrected comment in Unit::createFile().

--- src/unit/createfile.cpp
+++ src/unit/createfile.cpp
@@ -5,14 +5,14 @@
 
 #include <sys/stat.h>   // Required for stat
 
-StatusCode Unit::createFile(std::string fileName, Bitmask<AttachFlags> attachMode)
+StatusCode Unit::createFile (std::string fileName, Bitmask<AttachFlags> attachMode)
 {
     struct stat info;
 
     // Opening a file with write/update will cause existing files to be
     // overwritten, so check if a file with the specified name already
-    // exists and if that's the case ask the user if it is ok to overwrite
-    // the file.
+    // exists and if that's the case allow overwriting only if the Overwrite
+    // flag is specified.
     if (!stat (fileName.c_str(), &info) && !(attachMode & AttachFlags::Overwrite))
         return StatusCode::ArgumentError;
 
