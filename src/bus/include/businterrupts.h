f70fc75bb5aaf1a4c77e58823ce81592715ecd14
Jos Fries <jos.fries@kpnmail.nl>
2025-06-16 15:11:22 +0200

    Revert "Added Bus::busRequest() interface."
    This reverts commit 2600b644605e7b06451cf446bd3941c163aea97a.

--- src/bus/include/businterrupts.h
+++ src/bus/include/businterrupts.h
@@ -7,22 +7,10 @@
 
 using std::function;
 
-// This interfaces declares the bus functions for interrupt handling.
-// 
-// To set an interrupt request a device calls setInterrupt() and these
-// interrupts will then be handled in order of their priority. Bus arbitration
-// is thus performed implicitcly.
-// 
-// Bus arbitration can also be performed explicitly by calling busRequest().
-// This function includes a busGrant callback function wich will be called
-// when an interrupt request with the given bus request priority will be
-// honoured.
-//
+// This interfaces declares the bus functions for interrupt handling
 class BusInterrupts
 {
 public:
-	virtual void busRequest (TrapPriority priority, unsigned char busOrder,
-		u8 functionOrder, function<void ()> busGrant = 0) = 0;
 	virtual void setInterrupt (TrapPriority priority, unsigned char busOrder,
 		u8 functionOrder, unsigned char vector, function<void()> ack = 0) = 0;
 	virtual bool containsInterrupt (TrapPriority priority, unsigned char busOrder,
--- src/bus/qbus/businterrupts.cpp
+++ src/bus/qbus/businterrupts.cpp
@@ -2,13 +2,6 @@
 
 using std::function;
 
-void Qbus::busRequest (TrapPriority priority, unsigned char busOrder,
-	u8 functionOrder, function<void ()> busGrant)
-{
-
-
-}
-
 void Qbus::setInterrupt (TrapPriority priority, unsigned char busOrder,
 	u8 functionOrder, unsigned char vector, function<void ()> ack)
 {
