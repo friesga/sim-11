69dfc27f3a8c3541af2a727341b9d6eeb13d8ceb
Jos Fries <jos.fries@kpnmail.nl>
2025-06-24 08:45:31 +0200

    Moved setSeekComplete() to separate cpp-file.

--- src/rk/rk11d/processaction.cpp
+++ src/rk/rk11d/processaction.cpp
@@ -50,14 +50,3 @@ void RK11D::processFunction (RKTypes::Function function)
 }
 
 
-// The RK05 uses this function to indicate a seek function completed
-//
-void RK11D::setSeekComplete (RKTypes::DriveCondition condition)
-{
-    // Guard against controller register access from the RK11D thread
-    std::lock_guard<std::mutex> guard {controllerMutex_};
-
-    driveConditionQueue_.push (condition);
-    pollEventQueue_.push (SeekComplete {});
-}
-
