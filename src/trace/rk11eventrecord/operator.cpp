da62836d67d903da16d166edaf8f641c82ad6371
Jos Fries <jos.fries@kpnmail.nl>
2025-08-12 17:14:11 +0200

    Defined RK11Names::getFunctionName() and getStateName() as free functions.

--- src/trace/rk11eventrecord/operator.cpp
+++ src/trace/rk11eventrecord/operator.cpp
@@ -1,4 +1,5 @@
 #include "rk11eventrecord.h"
+#include "trace/rk11names/rk11names.h"
 
 #include <iomanip>
 #include <variant>
@@ -9,11 +10,14 @@ using std::oct;
 using std::dec;
 using std::holds_alternative;
 
+using RK11Names::getFunctionName;
+using RK11Names::getStateName;
+
 // Print the given record to the given output stream
 std::ostream& operator<< (std::ostream& os,
     TraceRecord<RK11EventRecord> record)
 {
-    os << "[RK11] State [" << record.getStateName (record.state_) << "] ";
+    os << "[RK11] State [" << getStateName (record.state_) << "] ";
 
     if (holds_alternative<RKTypes::SeekCompleteReport> (record.event_))
     {
@@ -26,14 +30,13 @@ std::ostream& operator<< (std::ostream& os,
     {
         RKTypes::Function function = 
             std::get<RKTypes::Function>(record.event_);
-        os << "Function [" << record.getFunctionName (function) <<
+        os << "Function [" << getFunctionName (function) <<
             " (" << function.rkcs.operation << ')' <<
             " RKCS=" << setw (7) << oct << function.rkcs.value <<
             " RKDA=" << setw (7) << oct << function.diskAddress.value <<
             " RKWC=" << function.wordCount <<
             " RKBA=" << setw (7) << oct << function.busAddress << "]\n";
     }
-
     else
         throw std::runtime_error ("Unknown event type");
     
