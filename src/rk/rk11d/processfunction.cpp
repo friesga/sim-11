f277f08a02955c648f1b6ce897bca9f83dc445e8
Jos Fries <jos.fries@kpnmail.nl>
2025-08-16 12:09:28 +0200

    Replaced all_of() by a range for loop to avoid lambda function.

--- src/rk/rk11d/processfunction.cpp
+++ src/rk/rk11d/processfunction.cpp
@@ -1,24 +1,30 @@
 #include "rk11d.h"
 
-#include <algorithm>
-
-using std::ranges::all_of;
-
+// This function executes the RK11 function by execution all steps
+// in the sequence for that function.
+// 
 // The function is already safeguarded against register access by the
-// CPU thread as the controllerMutex_ is locked by the action processor.
+// CPU thread as the controllerMutex_ is locked by the function processor.
 //
 void RK11D::processFunction (RKTypes::Function function)
 {
     RKTypes::CommandCompletion commandCompletion {};
     StepVector sequenceToBeExecuted {};
 
+    // The Read Header function is a special case in the sense that it
+    // doesn't have its own operation code, but is selected by the
+    // combination of the Read operation code and setting of the format
+    // bit.
     if (function.rkcs.operation == RKTypes::Read && function.rkcs.format)
         sequenceToBeExecuted = readHeaderFunction_;
     else
         sequenceToBeExecuted = rk11dFunctions[function.rkcs.operation];
 
-    all_of (sequenceToBeExecuted, [&] (auto& f)
-        { return f (function, commandCompletion); });
+    for (auto step : sequenceToBeExecuted)
+    {
+        if (!(step)(function, commandCompletion))
+            break;
+    }
 
     setControlReady ();
 }
