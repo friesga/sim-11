c1eeeab573caeb435e6548ad15c6e3295a55e97b
Jos Fries <jos.fries@kpnmail.nl>
2026-01-15 09:31:16 +0100

    Defined KA11CpuData as an AbstractBusDevice.

--- src/proc/ka11/ka11cpudata/ka11cpudata.h
+++ src/proc/ka11/ka11cpudata/ka11cpudata.h
@@ -6,19 +6,31 @@
 #include "ka11_psw.h"
 #include "types.h"
 #include "trace/trace.h"
+#include "abstractbusdevice/abstractbusdevice.h"
 
 //
 // The class KA11CpuData implements the CpuData interface for the KA11
 //
-class KA11CpuData : public BaseCpuData<SingularRegisterSet, KA11_PSW>
+class KA11CpuData : public BaseCpuData<SingularRegisterSet, KA11_PSW>,
+    public AbstractBusDevice
 {
 public:
-	// Functions required by the CpuData interface and not implemented by
-	// BaseCpuData.
-	constexpr bool stackOverflow () override;
+    // Functions required by the CpuData interface and not implemented by
+    // BaseCpuData.
+    constexpr bool stackOverflow () override;
+
+    // Functions required by the BusDevice interface and not implemented by
+    // AbstractBusDevice.
+    CondData<u16> read (BusAddress address) override;
+    StatusCode writeWord (BusAddress address, u16 value) override;
+    bool responsible (BusAddress address) override;
+    void reset () override;
+
+private:
+    enum { PSWAddress = 0177776 };
 };
 
-// The KD11-NA does not support a stack limit so stack overflow cannot occur.
+// The KA11 does not support a stack limit so stack overflow cannot occur.
 constexpr bool KA11CpuData::stackOverflow ()
 {
     return false;
