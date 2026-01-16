a1877ba56bf7df326de579b7465334e80966f994
Jos Fries <jos.fries@kpnmail.nl>
2026-01-16 11:31:36 +0100

    Added M792 to ConsistencyChecker::checkROMDeviceConsistency().

--- src/configdata/consistencychecker/checkromdeviceconsistency.cpp
+++ src/configdata/consistencychecker/checkromdeviceconsistency.cpp
@@ -10,21 +10,21 @@ using std::invalid_argument;
 using std::holds_alternative;
 using std::visit;
 
-// Check that either a M9312 or a KT24 and not both are specified in the
-// configuration
+// Check that one of M9312, KT24 or M792 is specfied in the configuration
 void ConsistencyChecker::checkROMDeviceConsistency ()
 {
     size_t numberOfROMS =
         count_if (systemConfig_, &ConsistencyChecker::findDevice<M9312Config>) +
-        count_if (systemConfig_, &ConsistencyChecker::findDevice<KT24Config>);
+        count_if (systemConfig_, &ConsistencyChecker::findDevice<KT24Config>) +
+        count_if (systemConfig_, &ConsistencyChecker::findDevice<M792Config>);
 
     // Verify just one ROM boot device is specified
     if (numberOfROMS > 1)
-        throw invalid_argument {"Double diagnostic/boot ROM device specified, specify either M9312 or KT24"};
+        throw invalid_argument {"Multiple diagnostic/boot ROM devices specified, specify one of M9312, KT24 or M792"};
 
     // Verify at least one ROM boot device is specified
     if (numberOfROMS == 0)
-        throw invalid_argument {"No diagnostic/boot ROM specified, specify M9312 or KT24"};
+        throw invalid_argument {"No diagnostic/boot ROM specified, specify M9312, KT24 or M792"};
 }
 
 
