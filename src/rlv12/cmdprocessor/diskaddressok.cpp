13cccb9e554e1b4d1d273ddee9cd2213b4b3d89f
Jos Fries <jos.fries@kpnmail.nl>
2026-05-07 08:52:07 +0200

    Corrected CmdProcessor::diskAddressOk().

--- src/rlv12/cmdprocessor/diskaddressok.cpp
+++ src/rlv12/cmdprocessor/diskaddressok.cpp
@@ -4,7 +4,7 @@ bool CmdProcessor::diskAddressOk (RL01_02 *unit, RLV12Command &rlv12Command)
 {
     if (RLV12const::getCylinder (unit->currentDiskAddress_) != 
             RLV12const::getCylinder (rlv12Command.diskAddress_) ||
-            RLV12const::getSector (unit->currentDiskAddress_) >= RLV12const::sectorsPerSurface)
+            RLV12const::getSector (rlv12Command.diskAddress_) >= RLV12const::sectorsPerSurface)
         // Bad cylinder or sector
         return false;
 
