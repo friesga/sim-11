ed19a1c6863cfb9829656b39fcc2258f6572f831
Jos Fries <jos.fries@kpnmail.nl>
2026-05-07 11:34:15 +0200

    Corrected CmdProcessor::readDataWithoutHeaderCheckCmd().

--- src/rlv12/cmdprocessor/readdatawithoutheadercheckcmd.cpp
+++ src/rlv12/cmdprocessor/readdatawithoutheadercheckcmd.cpp
@@ -2,8 +2,23 @@
 #include "logger/logger.h"
 
 //
-// Perform a Read Data Without Header Check command for the specified unit with the 
-// specified parameters.
+// Perform a Read Data Without Header Check command for the specified unit
+// with the  specified parameters.
+//
+// The read data without header check command is a special command used to
+// recover data from sectors with bad header information.Normally, header
+// information that has eRe errors prevents use of the data in that sector.
+// The method used to recover this data is to read header and seek to the
+// proper track if necessary. Then begin issuing successive read header
+// commands until the sector preceding the bad sector is located. The read
+// data without header check command must then be issued within 482
+// microseconds after the completion of the read header command.
+//
+// The absence of a header check implies that the disk address specified in
+// the DAR (and thus in the RLV12Command) has to be ignored. Diagnostic
+// ZRLHB1 test 40 e.g. uses this feature and specifies an illegal disk
+// address in the DAR for the command. The disk address to read from therefore
+// is the current disk address.
 //
 u16 CmdProcessor::readDataWithoutHeaderCheckCmd (RL01_02 *unit, 
     RLV12Command &rlv12Command)
@@ -31,6 +46,9 @@ u16 CmdProcessor::readDataWithoutHeaderCheckCmd (RL01_02 *unit,
 
     if (rlv12Command.wordCount_ > maxWordCount)
         rlv12Command.wordCount_ = maxWordCount;
+
+    // Read data from the current disk address
+    rlv12Command.diskAddress_ = unit->currentDiskAddress_;
 
     auto [success, numBytes] = unit->readData (rlv12Command,
         controller_->dataBuffer_, RL01_02::HeadPositionProcedure::Increment,
