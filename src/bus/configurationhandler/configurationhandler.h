53b46d51247e716c340ac26b368be420a762be26
Jos Fries <jos.fries@kpnmail.nl>
2026-05-12 10:40:10 +0200

    Increased the number of slots to 32.

--- src/bus/configurationhandler/configurationhandler.h
+++ src/bus/configurationhandler/configurationhandler.h
@@ -20,7 +20,7 @@ public:
 private:
 	// A BA11-N backplane has nine slots, named ROW 1 to ROW 9. ROW 1 corresponds
 	// with slot[0], ROW 9 with slot [8].
-	static const size_t numberOfSlots_ {9};
+	static const size_t numberOfSlots_ {32};
 	size_t numDevices_ {0};
 
 	BusDevice* slots_[numberOfSlots_] {nullptr};
